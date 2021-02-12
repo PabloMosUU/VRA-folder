8f2a966a07e0b2ad31d32a32f532c30c7ab61e06
Pablo Mosteiro <p.j.mosteiroromero@uu.nl>
2021-02-12 10:44:42 +0100

    integrals can be optimal or pessimistic

--- Metrics.py
+++ Metrics.py
@@ -6,64 +6,69 @@ Created on Thu Jan 28 11:19:48 2021
 """
 
 class Metrics:
-    def __init__(self, probabilities, labels):
+    def __init__(self, probabilities, labels, integral='trapezoid'):
         self.probabilities = probabilities
         self.labels = labels
-        self.thresholds = sorted(list(set(probabilities)))
+        self.integral = integral
+        assert integral in ['trapezoid','max','min'], '"'+str(integral)+'"'+ ' is not a valid integral value. Choose between "trapezoid", "min" or "max"'
+        self.probabilities_set = sorted(list(set(probabilities)))
     
     #make predictions based on the threshold value and self.probabilities
     def make_predictions(self, threshold):
-        return [int(prob>=threshold) for prob in self.probabilities]
+        predictions = []
+        for prob in self.probabilities:
+            if prob >= threshold:
+                predictions.append(1)
+            else: 
+                predictions.append(0)
+        return predictions
     
     #make list with kappa scores for each threshold
     def kappa_curve(self):
-        kappas = []        
-        for thres in self.thresholds:
+        kappa_list = []
+        
+        for thres in self.probabilities_set:
             preds = self.make_predictions(thres)
             tp, tn, fp, fn = self.confusion_matrix(preds)
             k = self.calc_kappa(tp, tn, fp, fn)
-            kappas.append(k)
-        return kappas
+            kappa_list.append(k)
+        return self.add_zero_to_curve(kappa_list)
     
     #make list with fpr scores for each threshold
     def fpr_curve(self):
         fpr_list = []
-
-        for thres in self.thresholds:
+        
+        for thres in self.probabilities_set:
             preds = self.make_predictions(thres)
             tp, tn, fp, fn = self.confusion_matrix(preds)
             fpr = self.calc_fpr(fp, tn)
             fpr_list.append(fpr)
-        return fpr_list
+        return self.add_zero_to_curve(fpr_list)
     
     def tpr_curve(self):
         tpr_list = []
-                
-        for thres in self.thresholds:
+               
+        for thres in self.probabilities_set:
             preds = self.make_predictions(thres)
             tp, _, _, fn = self.confusion_matrix(preds)
             tpr = self.calc_tpr(tp, fn)
             tpr_list.append(tpr)
-        return tpr_list
+        
+        return self.add_zero_to_curve(tpr_list)
 
     #make list with precision scores for each threshold
     def precision_curve(self):
         precision_list = []
         
-        for thres in self.thresholds:
+        for thres in self.probabilities_set:
             preds = self.make_predictions(thres)
             tp, _, fp, _ = self.confusion_matrix(preds)
             precision = self.calc_precision(tp, fp)
             precision_list.append(precision)
-        return precision_list
+        return self.add_one_to_curve(precision_list)
 
     #calculate confusion matrix
-    # Returns: TP, TN, FP, FN
     def confusion_matrix(self, predictions):
-        # Can replace for:
-        # (after import sklearn.metrics as m)
-        # tn, fp, fn, tp = m.confusion_matrix(self.labels, predictions).ravel()
-        # return tp, tn, fp, fn
         tp = 0
         tn = 0
         fp = 0
@@ -76,142 +81,135 @@ class Metrics:
                     tn += 1
             elif pred == 1:
                 fp += 1
-            else:
-                fn += 1
+            else: fn += 1
             tot = tp + tn + fp + fn
-            # Dividing by the total is not necessary in the confusion matrix
         return tp/tot, tn/tot, fp/tot, fn/tot
         
-    
-    
     #Calculate AUK
     def calc_auk(self):
-        # You can replace this method by
-        # (After import sklearn.metrics as m)
-        # cohen_kappas = [m.cohen_kappa_score(self.labels,
-        #                                     self.make_predictions(threshold))
-        #                 for threshold in thresholds]
-        # return m.auc(self.fpr_curve(), cohen_kappas)
+        
         auk=0
         fpr_list = self.fpr_curve()
-        
-        for i, prob in enumerate(self.thresholds[:-1]):
-            x_dist = abs(fpr_list[i+1] - fpr_list[i])
             
+        for i, prob in enumerate(self.probabilities_set[:-1]):
+            x_dist = abs(fpr_list[i+1] - fpr_list[i])
+                
             preds = self.make_predictions(prob) 
             tp, tn, fp, fn = self.confusion_matrix(preds)
             kapp1 = self.calc_kappa(tp, tn, fp, fn)
-            # All the calculations for +1 will be re-done in next iteration
-            # This is inefficient
-            preds = self.make_predictions(self.thresholds[i+1]) 
+                
+            preds = self.make_predictions(self.probabilities_set[i+1]) 
             tp, tn, fp, fn = self.confusion_matrix(preds)
             kapp2 = self.calc_kappa(tp, tn, fp, fn)
-            # There's not thing wrong with this, but I feel like you are
-            # reinventing the wheel if you write your own integrator
-            # The problem with this is that if you make a tiny mistake here,
-            # it's hard to catch because you have to go through a lot of code
-            # But if you re-use libraries, you know they have been tested
+                
             y_dist = abs(kapp2-kapp1)
-            top = (y_dist * x_dist)/2
             bottom = min(kapp1, kapp2)*x_dist
-            auk += top + bottom
+            auk += bottom
+            #print('AUK is: ',str(auk))
+            if self.integral is 'trapezoid':
+                top = (y_dist * x_dist)/2
+                auk += top
+                #print('Trapezoid is calculated. Top is area: ',str(top))
+            elif self.integral is 'max':
+                top = (y_dist * x_dist)
+                auk += top
+                #print('Max is calculated. Top is area: ',str(top))
+            else:
+                continue
+            #print('AUK after addition is: ',str(auk))
+            
         return auk
        
        
     #Calculate roc-auc
-    # It's not reasonable to write an integrator twice just because the x
-    # and y axis are different. You should have a calc_auc method that takes
-    # the x and y axes as parameters, and integrate there. Like this:
-    # def calc_auc(self, x, y):
-    #     auc = 0
-    #     for i in range(len(x)):
-    #         x_dist = abs(x[i+1] - x[i])
-    #         y_1 = y[i]
-    #         y_2 = y[i+1]
-    #         y_dist = abs(y_2 - y_1)
-    #         top = (y_dist * x_dist) / 2
-    #         bottom = x_dist * min(y_1, y_2)
-    #         auc += (top + bottom)
-    #     return auc
-    # In that case, the current method would be like this:
-    # def calc_roc_auc(self):
-    #     fpr_list = self.fpr_curve()
-    #     tpr_list = self.tpr_curve()
-    #     return self.calc_auc(fpr_list, tpr_list)
-    # This does mean iterating over the thresholds twice, but because the
-    # calc_auc method is fast, and because the thresholds are not that many,
-    # it will be faster, and easier to read
-    def calc_roc_auc(self):
+    def calc_roc_auc(self, integral='trapezoid'):
+        assert integral in ['trapezoid','max','min'], '"'+str(integral)+'"'+ ' is not a valid integral value. Choose between "trapezoid", "min" or "max"'
         roc_auc = 0
         fpr_list = self.fpr_curve()
+
         
-        for i, prob in enumerate(self.thresholds[:-1]):
+        for i, prob in enumerate(self.probabilities_set[:-1]):
             x_dist = abs(fpr_list[i+1] - fpr_list[i])
             
             preds = self.make_predictions(prob) 
-            tp, tn, fp, fn = self.confusion_matrix(preds)
+            tp, _, _, fn = self.confusion_matrix(preds)
             tpr1 = self.calc_tpr(tp, fn)
             
-            preds = self.make_predictions(self.thresholds[i+1]) 
+            preds = self.make_predictions(self.probabilities_set[i+1]) 
             tp, tn, fp, fn = self.confusion_matrix(preds)
             tpr2 = self.calc_tpr(tp, fn)
 
             y_dist = abs(tpr2-tpr1) 
-            top = (y_dist * x_dist)/2
             bottom = x_dist * min(tpr1, tpr2)
-            roc_auc += top + bottom
+            roc_auc += bottom
+            
+            if self.integral is 'trapezoid':
+                top = (y_dist * x_dist)/2
+                roc_auc += top
+            elif self.integral is 'max':
+                top = (y_dist * x_dist)
+                roc_auc += top
+            else:
+                continue
         return roc_auc
-
-    # Same comment as I wrote for calc_pr_auc
-    def calc_pr_auc(self):
+    
+    def calc_pr_auc(self, integral='trapezoid'):
+        assert integral in ['trapezoid','max','min'], '"'+str(integral)+'"'+ ' is not a valid integral value. Choose between "trapezoid", "min" or "max"'
         pr_auc = 0
         tpr_list = self.tpr_curve()
         
-        for i, prob in enumerate(self.thresholds[:-1]):
+        for i, prob in enumerate(self.probabilities_set[:-1]):
             x_dist = abs(tpr_list[i+1] - tpr_list[i])
             
             preds = self.make_predictions(prob) 
             tp, _, fp, _ = self.confusion_matrix(preds)
             precision1 = self.calc_precision(tp, fp)
              
-            preds = self.make_predictions(self.thresholds[i+1]) 
+            preds = self.make_predictions(self.probabilities_set[i+1]) 
             tp, _, fp, _ = self.confusion_matrix(preds)
             precision2 = self.calc_precision(tp, fp)
 
             y_dist = abs(precision2-precision1) 
-            top = (y_dist * x_dist)/2
             bottom = x_dist * min(precision1, precision2)
-            pr_auc += top + bottom
+            pr_auc += bottom
+            
+            if self.integral is 'trapezoid':
+                top = (y_dist * x_dist)/2
+                pr_auc += top
+            elif self.integral is 'max':
+                top = (y_dist * x_dist)
+                pr_auc += top
+            else:
+                continue
+        return pr_auc
+            #The code below seems unnecessary now that 
+        '''
+            if step is False:                    
+                top = (y_dist * x_dist)/2
+                pr_auc += top
         
+        if step is False:
+            #add begin area before smallest probability  
+            preds = self.make_predictions(min(self.probabilities_set))
+            tp, _, fp, _ = self.confusion_matrix(preds)
+            precision = self.calc_precision(tp, fp)
+            begin = (precision*min(tpr_list))/2
+            pr_auc += begin
         
-        #add begin area before smallest probability     
-        # Danger: tpr_list and self.thresholds may not be ordered in the same
-        # way, so taking the "min" of one and assuming it corresponds with the
-        # "min" of the other is risky
-        # A better solution is to rely on the fact that the thresholds are
-        # sorted (you sorted them in the constructor). Thus, you can write:
-        preds = self.make_predictions(self.thresholds[0])
-        tp, _, fp, _ = self.confusion_matrix(preds)
-        precision = self.calc_precision(tp, fp)
-        begin = (precision*tpr_list[0])/2
-        pr_auc += begin
-        # Note also that this relies on assuming that Precision(0,0)=0
-        # Do you understand why this is?
-        
-        #add end area after largest probability
-        # Danger: same as above; solution: same, but using "-1" instead of "0"
-        preds=self.make_predictions(self.thresholds[-1])
+        #add end area after largest probability 
+        preds=self.make_predictions(max(self.probabilities_set))
         tp, _, fp, _ = self.confusion_matrix(preds)
         precision = self.calc_precision(tp, fp)
         y_diff = 1-precision
-        x_diff= 1-tpr_list[-1]
-        end_top = (y_diff)*(x_diff)/2
+        x_diff= 1-max(tpr_list)
         end_bottom = precision * x_diff
-        pr_auc += end_top + end_bottom
+        pr_auc += end_bottom
+
+        if step is False:
+            end_top = (y_diff)*(x_diff)/2
+            pr_auc += end_top
         return pr_auc
-        # As before, note that this assumes Precision(max_threshold)=1
-        # Do you understand why this is?
-           
+        '''
         
     def calc_fpr(self, fp, tn):
         return fp/(fp+tn)
@@ -231,4 +229,19 @@ class Metrics:
         n_hat = fn + tn
         p_c = p * p_hat + n * n_hat
         return (acc - p_c) / (1 - p_c)    
+
+    #Add zero to appropriate position in list
+    def add_zero_to_curve(self, curve):
+        min_index = curve.index(min(curve)) 
+        if min_index> 0:
+            curve.append(0)
+        else: curve.insert(0,0)
+        return curve
     
+        #Add zero to appropriate position in list
+    def add_one_to_curve(self, curve):
+        max_index = curve.index(max(curve)) 
+        if max_index> 0:
+            curve.append(1)
+        else: curve.insert(0,1)
+        return curve
